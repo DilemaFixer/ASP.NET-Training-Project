2c2831b76cc51f000a608f80b17dd67099515f99
DilemaFixer <109586143+DilemaFixer@users.noreply.github.com>
2024-01-22 00:50:40 +0200

    that one init

---
+++ UdemProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UdemProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
