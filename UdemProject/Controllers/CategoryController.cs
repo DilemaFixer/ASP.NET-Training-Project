79190c44521e5f00758c8a69147d861dff7a5d8d
DilemaFixer <109586143+DilemaFixer@users.noreply.github.com>
2024-01-23 01:55:18 +0200

    Create category
    Create category in data base , add new page where i have table that show all data base conten

---
+++ UdemProject/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemProject.Data;
+
+namespace UdemProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext ApplicationDbContext;
+        public CategoryController(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+           List<Category> categories = ApplicationDbContext.Category.ToList();
+            return View(categories);
+        }
+    }
+}
--- UdemProject/obj/Debug/net8.0/UdemProject.AssemblyInfo.cs
+++ UdemProject/obj/Debug/net8.0/UdemProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UdemProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2831b76cc51f000a608f80b17dd67099515f99")]
 [assembly: System.Reflection.AssemblyProductAttribute("UdemProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UdemProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
