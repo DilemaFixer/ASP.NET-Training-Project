6acedcf25fe717eac40d5f9ceac12c6df8d766e0
DilemaFixer <109586143+DilemaFixer@users.noreply.github.com>
2024-01-24 16:36:44 +0200

    Work with data
    Add capability to add new data in db . Add a new page where u can enter data after create

--- UdemProject/Controllers/CategoryController.cs
+++ UdemProject/Controllers/CategoryController.cs
@@ -5,16 +5,29 @@ namespace UdemProject.Controllers
 {
     public class CategoryController : Controller
     {
-        private ApplicationDbContext ApplicationDbContext;
+        private ApplicationDbContext _applicationDbContext;
         public CategoryController(ApplicationDbContext applicationDbContext)
         {
-            ApplicationDbContext = applicationDbContext;
+            _applicationDbContext = applicationDbContext;
         }
 
         public IActionResult Index()
         {
-           List<Category> categories = ApplicationDbContext.Category.ToList();
+           List<Category> categories = _applicationDbContext.Category.ToList();
             return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _applicationDbContext.Category.Add(category);
+            _applicationDbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
